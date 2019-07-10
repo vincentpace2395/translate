fe8662dfe6842783ce971c96d5321d565e0280e4
Vincent Pace <vpace@nhpace.net>
2019-07-09 20:17:46 -0500

    Add Context to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language: language });
+    };
+
     render() {
         return (
             <div className='ui container'>
-                App
+                <div>
+                    Select a language:
+                    <i className='flag us' onClick={() => this.onLanguageChange('english')} />
+                    <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+                <LanguageContext.Provider value={this.state.language}>
+                    <ColorContext.Provider value='red'>
+                        <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageContext.Provider>
             </div>
         );
     }
